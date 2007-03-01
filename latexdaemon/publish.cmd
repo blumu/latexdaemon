1d11a62d587abaefe5fc52c4c9ebc477c0a7b256
William Blum <william.blum@gmail.com>
2007-03-01 17:54:19 +0000

    git-svn-id: http://latexdaemon.googlecode.com/svn@27 26509bd6-fa4b-0410-9a9a-d53cb929d59f

--- latexdaemon/publish.cmd
+++ latexdaemon/publish.cmd
@@ -2,6 +2,6 @@ copy Release\latexdaemon.exe d:\tools
 copy latexdaemon.zip d:\documents\website\software\latexdaemon
 copy changelog-inc.html d:\documents\website\software\latexdaemon
 pscp d:\documents\website\common\altsidebar.html william@www.famille-blum.org:public_html/common/
-rem pscp latexdaemon.zip william@www.famille-blum.org:public_html/softwares/latexdaemon
+rem pscp latexdaemon.zip william@www.famille-blum.org:public_html/software/latexdaemon
 pscp d:\documents\website\software\latexdaemon\* william@www.famille-blum.org:public_html/software/latexdaemon
 pause
