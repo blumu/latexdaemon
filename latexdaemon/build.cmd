566fcf9e1758194bd4bea10961e008a85288ab07
William Blum <william.blum@gmail.com>
2008-02-06 14:14:45 +0000

    git-svn-id: http://latexdaemon.googlecode.com/svn@177 26509bd6-fa4b-0410-9a9a-d53cb929d59f

--- latexdaemon/build.cmd
+++ latexdaemon/build.cmd
@@ -1,3 +1,5 @@
 ::set VSINSTALLDIR=D:\Microsoft Visual Studio 8\
+:: make sure the current directory is set to the directory containing this batch file
+cd /d %0\..
 "%VSINSTALLDIR%\Common7\IDE\devenv.com" /Rebuild Release latexdaemon.vcproj
 pause
