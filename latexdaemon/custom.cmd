2a380f3a788ec0a67fa66761fe3d8bc05bfadcdf
William Blum <william.blum@gmail.com>
2008-04-17 15:10:32 +0000

    git-svn-id: http://latexdaemon.googlecode.com/svn@204 26509bd6-fa4b-0410-9a9a-d53cb929d59f

---
+++ latexdaemon/custom.cmd
@@ -0,0 +1,12 @@
+@echo off
+REM Instruction:
+REM Launch latexdaemon with the following options:
+REM  -afterjob=custom 
+REM  -custom="\"C:\Path to this batch file\custom.cmd\""
+
+
+
+echo Executing custom command:
+echo -file name: %1
+echo -base name: 
+echo -extension:
