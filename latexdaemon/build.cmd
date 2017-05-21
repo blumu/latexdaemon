cb98252d253dbb7547fd092ba09d2f8067be94e4
William Blum <william.blum@gmail.com>
2017-05-20 23:59:06 -0700

    Upgrade from VS 2005 to VS2017

--- latexdaemon/build.cmd
+++ latexdaemon/build.cmd
@@ -1,4 +1,4 @@
 :: make sure the current directory is set to the directory containing this batch file
 cd /d %0\..
-%VSDEVENV% /Rebuild Release %PROJFILE%
+%VSDEVENV% /Rebuild Release latexdaemon.sln
 pause
--- latexdaemon/zip.cmd
+++ latexdaemon/zip.cmd
@@ -1,3 +1,7 @@
+:: Package build output for publication
+:: REQUIREMENT:
+::   7zip needs to be installed with:
+::    choco install 7zip.install
 @echo off
 :: change to directory containg this batch file
 cd /d %0\..
@@ -8,30 +12,16 @@ call getver.cmd
 echo Zipping LaTeXDaemon version %APP_VERSION%
 echo.
 
-del /q /s temp\
-rem if not exist temp mkdir temp
+if EXIST temp rd /q /s temp
+del builds\%PACKAGE_FILENAME%.zip 
+
 mkdir temp
-copy "%VSCRT%\*" temp
 copy Release\latexdaemon.exe temp
 copy custom.cmd temp
-goto zip
-:prepare_sources
-mkdir temp\src
-copy *.html temp\src
-copy *.cpp temp\src
-copy *.h temp\src
-copy *.vcproj temp\src
-copy *.sln temp\src
-copy *.cmd temp\src
-copy *.txt  temp\src
-:zip
-del builds\%PACKAGE_FILENAME%.zip 
 pushd temp
-7za -tzip -r a ../builds/%PACKAGE_FILENAME%.zip .
+7z -tzip -r a ../builds/%PACKAGE_FILENAME%.zip .
 popd
-del /q /s temp\
+
 rd /q /s temp
 
 endlocal
-
-::7za -tzip a latexdaemon.zip *.html *.cpp *.h *.vcproj *.sln *.cmd *.txt Release\latexdaemon.exe
