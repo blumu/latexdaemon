8b12edca33603d4003e9df181ee7b31633da4d5e
William Blum <william.blum@gmail.com>
2021-12-20 17:18:59 -0800

    Github Actions CI  (#38)
    Add Github Actions to build the solution

--- build/beforebuild.ps1
+++ build/beforebuild.ps1
@@ -1,17 +1,32 @@
 #.SYNOPSIS
-#    Patch version file with appveyor version numbers
-if(-not $Env:APPVEYOR_BUILD_VERSION) {
-    throw "Script need to run under appveyor"
+#    Patch version file with target version number
+param($releaseVersion)
+
+if ($releaseVersion) {
+    Write-Output "Using provided version number: $releaseVersion"
+} elseif ($Env:APPVEYOR_BUILD_VERSION) {
+    $releaseVersion = $Env:APPVEYOR_BUILD_VERSION
+    Write-Output "Using version number set by appveyor: $releaseVersion"
+} else {
+    throw "Script need to run under appveyor or version must be specified as parameter"
 }
 
-$version = $Env:APPVEYOR_BUILD_VERSION -split '\.'
+if ($releaseVersion -match '^\d+.\d+.\d+$') {
+    $tag = ''
+} else {
+    $tag = "-$releaseVersion"
+    $releaseVersion = "0.0.0"
+}
+
+$version = $releaseVersion -split '\.'
 
 $content = Get-Content $PSScriptRoot\..\latexdaemon\version.h2
 $content = $content | ForEach-Object {
     $_ `
         -replace '#define MAJOR_VERSION[ \t]*(.*)',"#define MAJOR_VERSION $($version[0])" `
         -replace '#define MINOR_VERSION[ \t]*(.*)',"#define MINOR_VERSION $($version[1])" `
-        -replace '#define BUILD[ \t]*(.*)',"#define BUILD $($version[2])"
+        -replace '#define BUILD[ \t]*(.*)',"#define BUILD $($version[2])" `
+        -replace '#define TAG[ \t]*(.*)', "#define TAG $($tag)"
 }
 Write-Host "Patching version info:"
 $content | Out-Host
@@ -21,5 +36,5 @@ $content | Set-Content -Path $PSScriptRoot\..\latexdaemon\version.h2
 Write-Host "Patching chocolatey nuspec file"
 
 $nuspec = Get-Content "$PSScriptRoot\..\choco\latexdaemon.nuspec" `
-| ForEach-Object { $_ -replace '<version>.*</version>',"<version>$($Env:APPVEYOR_BUILD_VERSION)</version>" }
+| ForEach-Object { $_ -replace '<version>.*</version>', "<version>$($releaseVersion)</version>" }
 $nuspec | Set-Content -Path "$PSScriptRoot\..\choco\latexdaemon.nuspec" -Encoding UTF8
--- latexdaemon/version.h
+++ latexdaemon/version.h
@@ -9,9 +9,9 @@
 #define MAKE_VERSION2_T(a,b,c)	MAKE_VERSION_T(a,b,c)
 #define APP_VERSION_T			MAKE_VERSION2_T(MAJOR_VERSION, MINOR_VERSION, BUILD)
 
-#define INFO_MAKE_VERSION_T(a,b,c)	_T("Version ") _T(#a) _T(".") _T(#b) _T(" Build ") _T(#c) 
-#define INFO_MAKE_VERSION2_T(a,b,c)	INFO_MAKE_VERSION_T(a,b,c)
-#define INFO_VERSION_T		        INFO_MAKE_VERSION2_T(MAJOR_VERSION, MINOR_VERSION, BUILD)
+#define INFO_MAKE_VERSION_T(a,b,c,d)   _T("Version ") _T(#a) _T(".") _T(#b) _T(" Build ") _T(#c) _T(#d)
+#define INFO_MAKE_VERSION2_T(a,b,c,d)  INFO_MAKE_VERSION_T(a,b,c,d)
+#define INFO_VERSION_T                 INFO_MAKE_VERSION2_T(MAJOR_VERSION, MINOR_VERSION, BUILD, TAG)
 
 
 // version number used in the version section of resource headers(.rc2)
@@ -20,5 +20,4 @@
 #define APP_TITLE			 "LatexDaemon"
 #define BUILD_DATE			__DATE__
 #define COMPANY_NAME		"William Blum"
-#define COPYRIGHT			  "Copyright © William Blum 2006-2009"
-
+#define COPYRIGHT			"Copyright ï¿½ William Blum 2006-2021"
